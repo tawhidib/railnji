b4175d99d9ebdc279196fbe2b7c75df1215e157d
tawhidib <tawhidib@gmail.com>
2025-11-25 07:27:44 +0600

    filmora page added

---
+++ app/filmography/page.tsx
@@ -0,0 +1,8 @@
+import FilmographyPageIndex from "@/components/pages/filmography";
+import { NextPage } from "next";
+
+const FilmographyPage: NextPage = () => {
+	return <FilmographyPageIndex />;
+};
+
+export default FilmographyPage;
---
+++ components/pages/filmography/index.tsx
@@ -0,0 +1,9 @@
+const FilmographyPageIndex = () => {
+	return (
+		<div>
+			<h1>Filmography</h1>
+		</div>
+	);
+};
+
+export default FilmographyPageIndex;
