51ed925007e632737997cfe966436389f6401df2
tawhidib <tawhidib@gmail.com>
2025-11-25 22:30:39 +0600

    hero section updated

--- components/pages/home/hero.tsx
+++ components/pages/home/hero.tsx
@@ -30,21 +30,21 @@ export default function LandingHeroArea() {
 				className="absolute inset-0 -z-10"
 			/>
 
-			<div className="col container pt-58 items-center">
-				<h1 className="text-center font-bold font-syne leading-[0.9] text-black uppercase text-[5rem]">
+			<div className="col container pt-45 md:pt-58 items-center">
+				<h1 className="text-center font-bold font-syne leading-[0.9] text-black uppercase lg:text-[5rem] text-4xl">
 					HELLO, I&apos;AM INJI
 				</h1>
-				<p className="mt-8 mb-15 text-center text-[1.75rem] leading-[1.4]">
+				<p className="mt-8 mb-15 text-center text-xl sm:text-2xl lg:text-[1.75rem] leading-[1.4]">
 					Producer & director who stays up to late to create
 					<br /> stories worth staying up late for
 				</p>
 
-				<div className="flex flex-col sm:flex-row items-center gap-10 justify-center">
+				<div className="flex flex-wrap items-center gap-4 md:gap-10 justify-center">
 					{navLinks.map((link) => (
 						<Link
 							key={link.name}
 							href={link.href}
-							className="uppercase font-medium text-text4 leading-loose font-poppins h-15 flex-center px-9 rounded-3xl hero-link"
+							className="uppercase font-medium text-text4 leading-loose font-poppins h-15 flex-center px-3 sm:px-5 md:px-9 rounded-3xl hero-link"
 						>
 							{link.name}
 						</Link>
